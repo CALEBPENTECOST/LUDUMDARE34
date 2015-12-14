763fc55e91b0650c4cfb03ac7d515888241915b1
Erin McBride <emcbride750@gmail.com>
2015-12-14 00:37:17 -0800

    added automatic y-leveling script to everything

--- unityproject/LD34/Assets/SpawnTaggableSpot.cs
+++ unityproject/LD34/Assets/SpawnTaggableSpot.cs
@@ -8,34 +8,34 @@ public class SpawnTaggableSpot : MonoBehaviour {
 	public float chance = 1.0f;
 	private GameObject spot = null;
 
-	private Vector2 getBox(GameObject obj){
-		var tmp = obj.gameObject.GetComponentInChildren<SpriteRenderer>().sprite.bounds.extents;
-		return new Vector2 (tmp.x*2.0f, tmp.y*2.0f);
-	}
-
 	// Use this for initialization
 	void Start () {
 		if (Random.Range (0.0f, 1.0f) < chance) {
-			Vector3 buildingBox = this.gameObject.GetComponentInChildren<SpriteRenderer>().sprite.bounds.extents;
+			//get list of tags
 			Object[] found = Resources.LoadAll (tagSpotFolder, typeof(GameObject));
-
-			//fisher-yates shuffle
+			//fisher-yates shuffle on tag list
 			for (int i = 0; i < found.Length; i++) {
 				int r = Random.Range (i, found.Length);
 				Object t = found [r];
 				found [r] = found [i];
 				found[i] = t;
 			}
+
 			//find first tag that will fit
+			Vector3 buildingBox = this.gameObject.GetComponentInChildren<SpriteRenderer>().sprite.bounds.extents;
+			float buildingHeight = buildingBox.y * 2.0f;
+
 			foreach (Object obj in found) {
 				GameObject tag = (GameObject)obj;
 				Vector2 tagBox = tag.GetComponent<BoxCollider2D> ().size;
-				if ((tagBox.y < buildingBox.y) && (tagBox.x < buildingBox.x)) {
-					float yOffset = Random.Range (tagBox.y / 2.0f, Mathf.Clamp((buildingBox.y - tagBox.y / 2.0f),0.0f,5.0f));
+				float tagHeight = tagBox.y;
+				float minAllowedTagHeight = tagHeight / 2.0f;
+				if ((tagHeight < buildingHeight) && (tagBox.x < buildingBox.x)) {
+					float yOffset = Random.Range (minAllowedTagHeight, Mathf.Clamp(buildingHeight - minAllowedTagHeight,minAllowedTagHeight,5.0f));
 					//float xOffset = Random.Range (-1.0f * (buildingBox.x - tagBox.x / 2.0f), (buildingBox.x - tagBox.x / 2.0f));
 
 					spot = Instantiate (tag, 
-						new Vector3 (this.transform.position.x, this.transform.position.y+yOffset, this.transform.position.z), Quaternion.identity) as GameObject;
+						new Vector3 (this.transform.position.x, yOffset, this.transform.position.z), Quaternion.identity) as GameObject;
 					break;
 				}
 			}
---
+++ unityproject/LD34/Assets/YLeveler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YLeveler : MonoBehaviour {
+
+
+	public float YOffset = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer r = this.gameObject.GetComponentInChildren<SpriteRenderer> ();
+		if (r != null) {
+			Vector3 spriteBox = r.sprite.bounds.extents;
+			Vector3 oldPos = this.transform.position;
+			//float scale = this.gameObject.transform.localScale.y;
+			this.transform.position = new Vector3 (oldPos.x, YOffset + spriteBox.y, oldPos.z);
+		}
+	}
+}
